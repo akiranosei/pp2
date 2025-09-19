f4b431fd020bb57d1a0561ff11c4fef6aa1a0f08
akiranosei <158202219+akiranosei@users.noreply.github.com>
2025-09-19 11:16:05 +0500

    Add files via upload

---
+++ lab1.py
@@ -0,0 +1,83 @@
+""" This is for multilimear
+#This is for one line
+
+python --version
+print ("Hello, World!")
+
+exit()
+
+x = 5
+y = 4
+print (5 + 4) 
+9
+
+if 5>2
+   print ("Five is greater than two!")
+
+print ("Hello","World")
+Hello World
+
+print ("Hello" + "World")
+HelloWorld
+
+var = 5
+my_var = 5 
+my-var = 5 (no)
+
+x = "John"
+x = 'John' are the same
+
+x = str(3) '3'
+x=int(3) 3
+x=float(3) 3.0
+
+x=y=z="orange"
+x,y,z = "apple", "banana", "orange"
+
+fruits = ["apples", "oranges", "bananas"]
+x,y,z = fruits
+
+x = "awesome"
+
+def myfunc():
+  print("Python is " + x)
+
+myfunc()
+(Python is awesome)
+
+x = "awesome"
+
+def myfunc():
+  x = "fantastic"
+  print("Python is " + x)
+
+myfunc()
+
+print("Python is " + x)
+
+Python is fantastic
+Python is awesome
+
+txt = "The best things in life are free!"
+if "free" in txt:
+  print("Yes, 'free' is present.")
+
+b = "Hello, World!"
+print(b[:6])
+Hello,
+
+b = "Hello, World!"
+print(b[-5:-2])
+orl
+
+hello = "Hello, World!"
+print(hello.upper())
+print(hello.lower())
+
+age = 97
+txt = f"My name is John, I am {age}"
+print(txt)
+
+
+
+"""
