e5420c215a7d8cfdc8f9dd491e6196d860a72480
akiranosei <158202219+akiranosei@users.noreply.github.com>
2025-11-07 00:06:20 +0500

    Add files via upload

---
+++ lab8.2.py
@@ -0,0 +1,105 @@
+import pygame
+import time
+import random
+pygame.init()
+snake_speed = 12
+height = 720
+width = 480
+black = pygame.Color(0, 0, 0)
+white = pygame.Color(255, 255, 255)
+green = pygame.Color(0, 255, 0)
+
+pygame.display.set_caption('Snake')
+game_window = pygame.display.set_mode((height, width))
+
+fps = pygame.time.Clock()
+snake_position = [100, 50]
+snake_body = [  [100, 50],
+                [100, 50],
+                [100, 50],
+                [100, 50] ]
+food_position = [random.randrange(1, (height//10)) * 10,
+                  random.randrange(1, (width//10)) * 10]
+food_spawn = True
+direction = 'DOWN'
+change_to = direction
+score = 0
+
+def show_score(choice, color, font, size):
+    score_font = pygame.font.SysFont(font, size)
+    score_surface = score_font.render('Score : ' + str(score), True, color)
+    score_rect = score_surface.get_rect()
+    game_window.blit(score_surface, score_rect)
+
+def game_over():
+    my_font = pygame.font.SysFont('times new roman', 40)
+    game_over_surface = my_font.render('Your Score is : ' + str(score), True, white)
+    game_over_rect = game_over_surface.get_rect()
+    game_over_rect.midtop = (height/2, width/4)
+    game_window.blit(game_over_surface, game_over_rect)
+    pygame.display.flip()
+    time.sleep(1)
+    pygame.quit()
+    quit()
+
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_UP:
+                change_to = 'UP'
+            if event.key == pygame.K_DOWN:
+                change_to = 'DOWN'
+            if event.key == pygame.K_LEFT:
+                change_to = 'LEFT'
+            if event.key == pygame.K_RIGHT:
+                change_to = 'RIGHT'
+
+    if change_to == 'UP' and direction != 'DOWN':
+        direction = 'UP'
+    if change_to == 'DOWN' and direction != 'UP':
+        direction = 'DOWN'
+    if change_to == 'LEFT' and direction != 'RIGHT':
+        direction = 'LEFT'
+    if change_to == 'RIGHT' and direction != 'LEFT':
+        direction = 'RIGHT'
+
+    if direction == 'UP':
+        snake_position[1] -= 10
+    if direction == 'DOWN':
+        snake_position[1] += 10
+    if direction == 'LEFT':
+        snake_position[0] -= 10
+    if direction == 'RIGHT':
+        snake_position[0] += 10
+
+    snake_body.insert(0, list(snake_position))
+    if snake_position[0] == food_position[0] and snake_position[1] == food_position[1]:
+        score += 1
+        food_spawn = False
+    else:
+        snake_body.pop()
+        
+    if not food_spawn:
+        food_position = [random.randrange(1, (height//10)) * 10, 
+                          random.randrange(1, (width//10)) * 10]
+        
+    food_spawn = True
+    game_window.fill(black)
+    
+    for pos in snake_body:
+        pygame.draw.rect(game_window, green, pygame.Rect(
+          pos[0], pos[1], 10, 10))
+        
+    pygame.draw.rect(game_window, white, pygame.Rect(
+      food_position[0], food_position[1], 12, 12))
+    
+    if snake_position[0] < 0 or snake_position[0] > height-10:
+        game_over()
+    if snake_position[1] < 0 or snake_position[1] > width-10:
+        game_over()
+    for block in snake_body[1:]:
+        if snake_position[0] == block[0] and snake_position[1] == block[1]:
+            game_over()
+    show_score(1, white, 'times new roman', 25)
+    pygame.display.update()
+    fps.tick(snake_speed)
---
+++ lab8.py
@@ -0,0 +1,165 @@
+#import all importnant libraries
+import pygame, sys
+from pygame.locals import *
+import random, time
+#initialize pygame library
+pygame.init()
+#set FPS, used colors and characteristics
+FPS = 60
+FramePerSec = pygame.time.Clock()
+RED   = (255, 0, 0)
+BLACK = (0, 0, 0)
+WHITE = (255, 255, 255)
+SCREEN_WIDTH = 400
+SCREEN_HEIGHT = 600
+SPEED = 5
+COINS = 0
+#set font style, game over text
+font = pygame.font.SysFont("Verdana", 60)
+font_small = pygame.font.SysFont("Verdana", 20)
+game_over = font.render("Game Over", True, BLACK)
+#set background picture and its size (adjust to window size)
+background = pygame.image.load("background.png")
+background = pygame.transform.scale(background, (SCREEN_WIDTH, SCREEN_HEIGHT))
+DISPLAYSURF = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+pygame.display.set_caption("Game")
+
+#enemy class type and how it can appear and move
+class Enemy(pygame.sprite.Sprite):
+    def __init__(self):
+        super().__init__() 
+        self.image = pygame.image.load("bullet.png")
+        self.image = pygame.transform.scale(self.image, (20, 20))
+        self.rect = self.image.get_rect()
+        self.rect.center = (random.randint(40, SCREEN_WIDTH - 40), 0)
+ 
+    def move(self):
+        global SCORE
+        self.rect.move_ip(0, SPEED)
+        if self.rect.top > SCREEN_HEIGHT:
+            self.rect.top = 0
+            self.rect.center = (random.randint(40, SCREEN_WIDTH - 40), 0)
+
+#coin class, where set its look and random appearance
+class Coin(pygame.sprite.Sprite):
+    def __init__(self):
+        super().__init__()
+        self.image = pygame.image.load("coin.png").convert_alpha()
+        self.image = pygame.transform.scale(self.image, (25, 25))
+        self.rect = self.image.get_rect()
+        self.reset_position()
+
+    def reset_position(self):
+        self.rect.center = (
+            random.randint(40, SCREEN_WIDTH - 40),
+            random.randint(100, SCREEN_HEIGHT - 100))
+
+    def move(self):
+        pass
+
+#set player (rocket) sizes, its movement across the screen, speed and borders
+class Player(pygame.sprite.Sprite):
+    def __init__(self):
+        super().__init__() 
+        self.image = pygame.image.load("rocket.png")
+        self.rect = self.image.get_rect()
+        self.rect.center = (160, 520)
+        
+    def move(self):
+        pressed_keys = pygame.key.get_pressed()
+        if pressed_keys[K_UP]:
+            self.rect.move_ip(0, -10)
+        if pressed_keys[K_DOWN]:
+            self.rect.move_ip(0, 10)
+        if pressed_keys[K_LEFT]:
+            self.rect.move_ip(-10, 0)
+        if pressed_keys[K_RIGHT]:
+            self.rect.move_ip(10, 0)
+        if self.rect.left < 0:
+            self.rect.left = 0
+        if self.rect.right > SCREEN_WIDTH:
+            self.rect.right = SCREEN_WIDTH
+        if self.rect.top < 0:
+            self.rect.top = 0
+        if self.rect.bottom > SCREEN_HEIGHT:
+            self.rect.bottom = SCREEN_HEIGHT
+
+#init classes and add character sprites
+P1 = Player()
+E1 = Enemy()
+C1 = Coin()
+enemies = pygame.sprite.Group()
+enemies.add(E1)
+coins = pygame.sprite.Group()
+coins.add(C1)
+all_sprites = pygame.sprite.Group()
+all_sprites.add(P1)
+all_sprites.add(E1)
+all_sprites.add(C1)
+INC_SPEED = pygame.USEREVENT + 1
+pygame.time.set_timer(INC_SPEED, 1000)
+
+#while game is gaming, ensure the constant speed, sprites/pics updating
+while True:
+    for event in pygame.event.get():
+        if event.type == INC_SPEED:
+            SPEED += 0.5
+        if event.type == QUIT:
+            pygame.quit()
+            sys.exit()
+
+    DISPLAYSURF.blit(background, (0, 0))
+
+    for entity in all_sprites:
+        entity.move()
+        DISPLAYSURF.blit(entity.image, entity.rect)
+
+    #if enemies crash wirh rocket, turn on crash sound and show the game over screen
+    #If user pushed R, the game restarts with 0 scroing, if user pushed Q game is over and window closes
+    if pygame.sprite.spritecollideany(P1, enemies):
+        pygame.mixer.Sound('crashsound.mp3').play()
+        time.sleep(0.5)
+        DISPLAYSURF.fill(RED)
+        DISPLAYSURF.blit(game_over, (30, 250))
+        restart_text = font_small.render("Press R to Restart or Q to Quit", True, BLACK)
+        DISPLAYSURF.blit(restart_text, (50, 350))
+        pygame.display.update()
+        for entity in all_sprites:
+            entity.kill()
+        waiting = True
+        while waiting:
+            for event in pygame.event.get():
+                if event.type == QUIT:
+                    pygame.quit()
+                    sys.exit()
+                if event.type == pygame.KEYDOWN:
+                    if event.key == pygame.K_r:
+                        SPEED = 5
+                        COINS = 0
+                        P1 = Player()
+                        E1 = Enemy()
+                        C1 = Coin()
+                        enemies.empty()
+                        enemies.add(E1)
+                        coins.empty()
+                        coins.add(C1)
+                        all_sprites.empty()
+                        all_sprites.add(P1)
+                        all_sprites.add(E1)
+                        all_sprites.add(C1)
+                        waiting = False
+                    elif event.key == pygame.K_q:
+                        pygame.quit()
+                        sys.exit()
+
+    #If receive coins, add coin get sound and add +1 to coin scoring, next coin should appear randomly
+    if pygame.sprite.spritecollideany(P1, coins):
+        pygame.mixer.Sound('coinget.mp3').play()
+        COINS += 1
+        C1.reset_position()
+
+    #coins scoring text appears at the right top side of screen
+    coins_text = font_small.render("Coins: " + str(COINS), True, WHITE)
+    DISPLAYSURF.blit(coins_text, (SCREEN_WIDTH - 120, 10))
+    pygame.display.update()
+    FramePerSec.tick(FPS)
